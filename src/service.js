a41397befebd2d55cc4943910316c66310fceed9
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-02-23 11:47:39 -0800

    add favoritePattern + fetchPattern function

--- src/service.js
+++ src/service.js
@@ -18,9 +18,37 @@ export function fetchFriends() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve([
-        { id: 1, name: "Foo" },
-        { id: 2, name: "Bar" },
+        { id: 1, name: "John Conway", favoritePattern: 123 },
+        { id: 3, name: "Murray Gell-Mann", favoritePattern: undefined },
+        { id: 4, name: "Mary Cartwright", favoritePattern: 456 },
       ]);
+    }, 2000);
+  });
+}
+
+// returns a Promise that eventually resolves to the pattern with the given ID
+// if no matching pattern found resolves to undefined
+export function fetchPattern(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const pattern = {
+        123: [
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 0],
+        ],
+        456: [
+          [0, 1, 0],
+          [0, 0, 1],
+          [1, 1, 1],
+        ],
+        789: [
+          [1, 1],
+          [1, 1],
+        ],
+      }[id];
+
+      resolve(pattern);
     }, 2000);
   });
 }
